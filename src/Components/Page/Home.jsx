3c4b0554844e281e464ce3da63e864191ae2c503
nasimuk47 <nasim.uk47@gmail.com>
2023-11-05 00:10:51 +0600

    add home page banner

---
+++ src/Components/Page/Home.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import PopolerService from "./PopolerService";
+
+const Home = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <div
+                    className="hero h-[560px] relative"
+                    style={{
+                        backgroundImage:
+                            "url(https://i.ibb.co/HnJCKBg/banner2.jpg)",
+                    }}>
+                    <div className="absolute left-24 top-48 space-y-5">
+                        <h1
+                            data-aos="slide-left"
+                            className="text-2xl font-bold text-gray-500">
+                            NEW ARRIVAL
+                        </h1>
+                        <h2
+                            data-aos="slide-right"
+                            className="text-5xl font-bold text-gray">
+                            Unlimited <br /> Clothes
+                        </h2>
+                        <div data-aos="slide-left">
+                            <button className="btn btn-primary flex justify-end bg-red-500 text-white">
+                                Shop Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <PopolerService></PopolerService>
+        </div>
+    );
+};
+
+export default Home;
